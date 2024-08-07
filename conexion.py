791694860097fdec0cc65f12a4e4ba2c784ea5b8
DeiviHerreraDiaz09 <herreradiazdeivistiven2004@gmail.com>
2024-08-06 20:43:58 -0500

    FEAT: Database modification

--- conexion.py
+++ conexion.py
@@ -33,11 +33,11 @@ class Conexion:
             )
             self.con = sqlite3.connect(db_path)
             self.creartablas()
-            self.crearTriggers()
-            self.crearAdmin()
-            self.consultar_y_crear_facturas()
-            self.actualizar_grado_estudiantes()
-            self.actualizar_estado_facturas()
+            # self.crearTriggers()
+            # self.crearAdmin()
+            # self.consultar_y_crear_facturas()
+            # self.actualizar_grado_estudiantes()
+            # self.actualizar_estado_facturas()
         except sqlite3.Error as e:
             print(f"Error al conectar con la base de datos: {e}")
         except Exception as ex:
@@ -51,9 +51,32 @@ class Conexion:
                 CREATE TABLE IF NOT EXISTS users (
                     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     user_name TEXT NOT NULL,
-                    user_email TEXT NOT NULL,
+                    user_email TEXT NOT NULL, 
                     password TEXT NOT NULL,
                     role TEXT NOT NULL
+                )
+            """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS monitoring_user (
+                    monitoring_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_id_fk INTEGER NOT NULL,
+                    monitoring_date TEXT NOT NULL,
+                    action TEXT NOT NULL,
+                    FOREIGN KEY (user_id_fk) REFERENCES users(user_id)
+                )
+            """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS configurations (
+                    configuration_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    school_name TEXT NOT NULL,
+                    school_address TEXT NOT NULL,
+                    school_phone TEXT NOT NULL,
+                    school_mora TEXT NOT NULL, 
+                    school_nfc TEXT NOT NULL
                 )
             """
             )
@@ -63,7 +86,6 @@ class Conexion:
                     student_ident TEXT PRIMARY KEY,
                     student_name TEXT NOT NULL,
                     date_of_birth TEXT NOT NULL,
-                    grade TEXT NOT NULL,
                     tutor_dni TEXT NOT NULL,
                     year_progress INTEGER NOT NULL,
                     tutor_name TEXT NOT NULL,
@@ -98,6 +120,63 @@ class Conexion:
                     payment_method TEXT NOT NULL,
                     invoice_id_fk INTEGER NOT NULL,
                     FOREIGN KEY (invoice_id_fk) REFERENCES invoices(invoice_id)
+                )
+            """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS periods (
+                    period_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    initial_period TEXT NOT NULL,
+                    final_period TEXT NOT NULL
+                    )
+            """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS rates (
+                    rate_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    rate_name TEXT NOT NULL,
+                    rate_amount TEXT NOT NULL
+                    )
+            """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS enrollments (
+                    enrollment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    enrollment_date TEXT NOT NULL,
+                    status TEXT NOT NULL,
+                    grade TEXT NOT NULL,
+                    enrollment_amount INTEGER NOT NULL,
+                    rate_id_fk INTEGER NOT NULL,
+                    period_id_fk INTEGER NOT NULL,
+                    student_id_fk INTEGER NOT NULL,
+                    FOREIGN KEY (rate_id_fk) REFERENCES rates(rate_id)
+                    FOREIGN KEY (period_id_fk) REFERENCES periods(period_id )
+                    FOREIGN KEY (student_id_fk) REFERENCES users(user_id)
+                )
+            """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS expenses (
+                    expense_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    description TEXT NOT NULL,
+                    total_amount INTEGER NOT NULL,
+                    created_at TEXT NOT NULL 
+                )
+            """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS cash_closings (
+                    cash_closing_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    cash_closing_date TEXT NOT NULL,
+                    cash_closing_enrollment_amount INTEGER NOT NULL,
+                    cash_closing_payments_amount INTEGER NOT NULL,
+                    cash_closing_expenses_amount INTEGER NOT NULL,
+                    total_amount_closing INTEGER NOT NULL 
                 )
             """
             )
